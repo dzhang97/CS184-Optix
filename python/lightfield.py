e46fc92776ebfc3cdfd0422066e1de0c72740cde
Denis Li <denis.li@berkeley.edu>
2018-04-17 16:37:45 -0700

    bilerp

--- python/lightfield.py
+++ python/lightfield.py
@@ -1,26 +1,9 @@
 from scipy import misc
 import numpy as np
 
-def shift_image(image, x, y):
-    temp = np.zeros_like(image, dtype=np.float64)
-    if x < 0:
-        temp[:,:x] = image[:,-x:]
-    elif x > 0:
-        temp[:,x:] = image[:,:-x]
-    else:
-        temp = image
-    ret = np.zeros_like(temp, dtype=np.float64)
-    if y < 0:
-        ret[:y] = temp[-y:]
-    elif y > 0:
-        ret[y:] = temp[:-y]
-    else:
-        ret = temp
-    return ret;
-
 class LightField:
     
-    def __init__(self, path_prefix, n, width=480, height=360):
+    def __init__(self, path_prefix, n, width=480, height=360, bilerp=True):
         self.images = [misc.imread(path_prefix + str(i) + ".png") for i in range(n**2)]
         self.width = width
         self.height = height
@@ -29,6 +12,7 @@ class LightField:
         middle = np.array([int(self.n/2), int(self.n/2)])
         for i in range(n**2):
             self.pos[i] = np.array([i % self.n, i / self.n]) - middle
+        self.bilerp = bilerp
 
     def get_square(self):
         square = []
@@ -73,13 +57,56 @@ class LightField:
             image.append(row)
         return image
 
+    def shift_image(self, i, x, y):
+        image = self.images[i]
+        temp = np.zeros_like(image, dtype=np.float64)
+        if x < 0:
+            temp[:,:x] = image[:,-x:]
+        elif x > 0:
+            temp[:,x:] = image[:,:-x]
+        else:
+            temp = image
+        ret = np.zeros_like(temp, dtype=np.float64)
+        if y < 0:
+            ret[:y] = temp[-y:]
+        elif y > 0:
+            ret[y:] = temp[:-y]
+        else:
+            ret = temp
+        return ret
+
+    def bilerp_shift(self, pos, shiftx, shifty):
+        shift_x_int = int(shiftx)
+        shift_y_int = int(shifty)
+        rx = shiftx - shift_x_int
+        ry = shifty - shift_y_int
+        if shiftx < 0:
+            rx = 1 + rx
+        if shifty < 0:
+            ry = 1 + ry
+        if shiftx >= 0:
+            c0 = (1-rx) * self.shift_image(pos, shift_x_int, shift_y_int) \
+                + rx * self.shift_image(pos, shift_x_int + 1, shift_y_int)
+            c1 = (1-rx) * self.shift_image(pos, shift_x_int, shift_y_int + 1) \
+                + rx * self.shift_image(pos, shift_x_int + 1, shift_y_int + 1)
+        else:
+            c0 = (1-rx) * self.shift_image(pos, shift_x_int - 1, shift_y_int - 1) \
+                + rx * self.shift_image(pos, shift_x_int, shift_y_int - 1)
+            c1 = (1-rx) * self.shift_image(pos, shift_x_int - 1, shift_y_int) \
+                + rx * self.shift_image(pos, shift_x_int, shift_y_int)
+        return (1-ry)*c0 + ry*c1
+
     def refocus(self, f):
         image = np.zeros(self.images[0].shape)
         shifty = -(f*(self.n-1)/2)
         for j in range(self.n):
             shiftx = -(f*(self.n-1)/2)
             for i in range(self.n):
-                image += shift_image(self.images[j*self.n + i], shiftx, shifty)
+                pos = j*self.n + i
+                if self.bilerp:
+                    image += self.bilerp_shift(pos, shiftx, shifty)
+                else:
+                    image += self.shift_image(pos, int(shiftx), int(shifty))
                 shiftx += f
             shifty += f
         return np.round(image / self.n**2).astype(np.uint8)
@@ -87,13 +114,13 @@ class LightField:
 if __name__ == '__main__':
     from matplotlib import pyplot as plt
     from matplotlib.widgets import Slider
-    lf = LightField("images/dragon_04_", 3, 480, 360)
+    lf = LightField("images/dragon_10_", 3, 480, 360)
     fig = plt.figure()
     ax = fig.add_subplot(111)
     fig.subplots_adjust(bottom=.15)
     ax.imshow(lf.refocus(0))
-    shift = Slider(fig.add_axes([0.15, .05, .75, .03]), 'Pixel Shift', -10, 10, valinit=0, valstep=1)
+    shift = Slider(fig.add_axes([0.15, .05, .75, .03]), 'Pixel Shift', -10, 10, valinit=0.1, valstep=.1)
     def update(val):
-        ax.imshow(lf.refocus(int(val)))
+        ax.imshow(lf.refocus(val))
     shift.on_changed(update)
     plt.show()
